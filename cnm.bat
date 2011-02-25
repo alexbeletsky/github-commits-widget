f986d1a6259e93a40ff66d0f6fb5310b25e910ca
alexander.beletsky <alexander.beletsky@gmail.com>
2011-02-25 13:47:25 +0200

    initial version

---
+++ cnm.bat
@@ -0,0 +1,6 @@
+echo off
+echo Combining and minifying .js files...
+
+cd ./js/mylibs
+"D:\Program Files\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m combine github.js md5.js github.commits.widget.js
+"D:\Program Files\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m jsmin github.commits.widget-min.js
