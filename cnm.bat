bac490cbb8debe34655d3b5547774480abc94e0c
Nithin Philips <nithin@nithinphilips.com>
2012-01-11 04:08:34 -0500

    Removing header tags from generated html.

--- cnm.bat
+++ cnm.bat
@@ -2,5 +2,5 @@ echo off
 echo Combining and minifying .js files...
 
 cd ./js/
-"D:\Program Files\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m combine github.js md5.js github.commits.widget.js
-"D:\Program Files\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m jsmin github.commits.widget-min.js
+"%PROGRAMFILES(X86)%\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m combine github.js md5.js github.commits.widget.js
+"%PROGRAMFILES(X86)%\SmallSharpTools\Packer for .NET\bin\Packer.exe" -o github.commits.widget-min.js -m jsmin github.commits.widget-min.js
